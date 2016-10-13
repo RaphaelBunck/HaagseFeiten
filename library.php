198090995f21d4e04e682e4ff77f5d5902a6b09b
Raphaël Bunck <raphaelbunck@gmail.com>
2016-10-13 10:07:43 +0200

    Copied master to dev (Only develop in /Dev until working)

---
+++ library.php
@@ -0,0 +1,17 @@
+<?php
+
+function navbar(){
+  echo "    <nav class='navbar navbar-default'>
+        <div class='container-fluid'>
+          <div class='navbar-header'>
+            <a class='navbar-brand' href='#'>Haagse Feiten</a>
+          </div>
+          <ul class='nav navbar-nav'>
+            <li class='active'><a href='#'>Home</a></li>
+          <li><a href='#'>Page 1</a></li>
+          <li><a href='#'>Contact</a></li>
+          <li><a href='#'>Page 3</a></li>
+        </ul>
+      </div>
+    </nav>";
+}
