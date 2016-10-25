6b064d1e04df51fa992d7a31a9c32f22bc38aef9
quinten1333 <quintencoltof1@gmail.com>
2016-10-25 17:31:24 +0200

    weg met smarty en oude structuur hersteld. gebruik index.php als een guide. daar heb ik alles gecomment waarom enz.

--- index.php
+++ index.php
@@ -1,39 +1,56 @@
-<?php
-/**
- * Example Application
- *
- * @package Example-application
- */
-
-require 'smarty/Smarty.class.php';
-
-$smarty = new Smarty;
-
-//$smarty->force_compile = true;
-$smarty->debugging = true;
-$smarty->caching = true;
-$smarty->cache_lifetime = 120;
+<?php include('navbar.php') // hier staan alle head variabelen, variabelen die standaard geinporteert moeten worden, beveiliging scripts enz. en de navbar?> 
 
 
-if(!$_GET['site']){
-/*$smarty->assign("Name", "Fred Irving Johnathan Bradley Peppergill", true);
-$smarty->assign("FirstName", array("John", "Mary", "James", "Henry"));
-$smarty->assign("LastName", array("Doe", "Smith", "Johnson", "Case"));
-$smarty->assign("Class", array(array("A", "B", "C", "D"), array("E", "F", "G", "H"), array("I", "J", "K", "L"),
-                               array("M", "N", "O", "P")));
+<div id="banner">
+  <img src="http://www.centrumeenenal.nl/wp-content/uploads/2012/07/blije-mensen.jpg">
+</div>
 
-$smarty->assign("contacts", array(array("phone" => "1", "fax" => "2", "cell" => "3"),
-                                  array("phone" => "555-4444", "fax" => "555-3333", "cell" => "760-1234")));
+<div class="container">
 
-$smarty->assign("option_values", array("NY", "NE", "KS", "IA", "OK", "TX"));
-$smarty->assign("option_output", array("New York", "Nebraska", "Kansas", "Iowa", "Oklahoma", "Texas"));
-$smarty->assign("option_selected", "NE");*/
-//$smarty->assign('','');
-  $smarty->display('index.tpl');
-} elseif ($_GET['site'] == 'zoeken'){
-  $smarty->display('zoeken.tpl');
-}
+  <div class="row">
+    <div class="col-sm-6 main" id="summary">
+      <h3>Wat is Haagse Feiten?</h3>
+      <p>Haagse Feiten is een digitale dienst die toegang biedt tot parlementaire informatie die verrijkt wordt met data en informatie uit diverse relevante bronnen. Bronnen zoals:<p>
+
+        <p>Overheidsbronnen <br>
+          Nieuwsbronnen;<br>
+          Social media-kanalen.
+        </p>
+
+        Je hoeft dus nooit meer tientallen websites te bezoeken, want alle relevante informatie wordt in één oogopslag helder en inzichtelijk gepresenteerd door middel van een persoonlijk dashboard.<br>
+
+        Wij richten ons op bedrijven, instellingen, overheden en professionals voor wie de politieke besluitvorming én het proces van invloed zijn op hun business, beleid of praktijk. Haagse Feiten faciliteert om dit reilen en zeilen goed te kunnen monitoren, dan wel te beïnvloeden.<br>
+
+        Haagse Feiten is dé toegangspoort waarachter alle beschikbare overheidsinformatie eenvoudig en snel terug te vinden is.</p>
+      </div>
 
 
-//$smarty->testInstall();
-?>
+      <div class="col-sm-6 main" id="fom">
+        <h3>Feit van de maand!</h3>
+        <img src="https://haagsefeiten.nl/wp-content/uploads/2016/05/marcelHenriquez.jpg">
+        <p><b>Wist je dat:</b> dit de oprichter is van Haagse Feiten?</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="footer">
+    <div class="container-fluid">
+      <ul id="footertext">
+        <li><a href="#">Voorwaarden</a></li>
+        <li>|</li>
+        <li><a href="#">Disclaimer & Copyright</a></li>
+        <li>|</li>
+        <li><a href="#">Privacy</a></li>
+        <li>|</li>
+        <li><a href="#">Adverteren</a></li>
+        <li>|</li>
+        <li><a href="#">Cookies</a></li>
+        <li>|</li>
+        <li><a href="#">FAQ</a></li>
+      </ul>
+      <ul id="copyright">
+        <li>&#169;Copyright 2016</li>
+    </div>
+  </div>
+  </body>
+  </html>
