224a60e8cff03c124390a8cd72699f3e9c91c2d1
quinten1333 <quintencoltof1@gmail.com>
2016-10-25 18:39:43 +0200

    structuur reset. gebruik index.php als template

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php include('navbar.php') // hier staan alle head variabelen, variabelen die standaard geinporteert moeten worden, beveiliging scripts enz. en de navbar?> 
+
+
+<div id="banner">
+  <img src="http://www.centrumeenenal.nl/wp-content/uploads/2012/07/blije-mensen.jpg">
+</div>
+
+<div class="container">
+
+  <div class="row">
+    <div class="col-sm-6 main" id="summary">
+      <h3>Wat is Haagse Feiten?</h3>
+      <p>Haagse Feiten is een digitale dienst die toegang biedt tot parlementaire informatie die verrijkt wordt met data en informatie uit diverse relevante bronnen. Bronnen zoals:<p>
+
+        <p>Overheidsbronnen <br>
+          Nieuwsbronnen;<br>
+          Social media-kanalen.
+        </p>
+
+        Je hoeft dus nooit meer tientallen websites te bezoeken, want alle relevante informatie wordt in één oogopslag helder en inzichtelijk gepresenteerd door middel van een persoonlijk dashboard.<br>
+
+        Wij richten ons op bedrijven, instellingen, overheden en professionals voor wie de politieke besluitvorming én het proces van invloed zijn op hun business, beleid of praktijk. Haagse Feiten faciliteert om dit reilen en zeilen goed te kunnen monitoren, dan wel te beïnvloeden.<br>
+
+        Haagse Feiten is dé toegangspoort waarachter alle beschikbare overheidsinformatie eenvoudig en snel terug te vinden is.</p>
+      </div>
+
+
+      <div class="col-sm-6 main" id="fom">
+        <h3>Feit van de maand!</h3>
+        <img src="https://haagsefeiten.nl/wp-content/uploads/2016/05/marcelHenriquez.jpg">
+        <p><b>Wist je dat:</b> dit de oprichter is van Haagse Feiten?</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="footer">
+    <div class="container-fluid">
+      <ul id="footertext">
+        <li><a href="#">Voorwaarden</a></li>
+        <li>|</li>
+        <li><a href="#">Disclaimer & Copyright</a></li>
+        <li>|</li>
+        <li><a href="#">Privacy</a></li>
+        <li>|</li>
+        <li><a href="#">Adverteren</a></li>
+        <li>|</li>
+        <li><a href="#">Cookies</a></li>
+        <li>|</li>
+        <li><a href="#">FAQ</a></li>
+      </ul>
+      <ul id="copyright">
+        <li>&#169;Copyright 2016</li>
+    </div>
+  </div>
+  </body>
+  </html>
