d4ea1398d77f4a2145f3f4dd252fef7d1aefcdd5
wippopo <peterveen11@gmail.com>
2016-10-25 20:21:21 +0200

    Add files via upload
    UPDATE

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
-<?php include('navbar.php') // hier staan alle head variabelen, variabelen die standaard geinporteert moeten worden, beveiliging scripts enz. en de navbar?> 
+<!DOCTYPE html>
+<html>
+<head>
+<title>Haagse Feiten</title>
+<link type="text/css" rel="stylesheet" href="css/index.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+
+<body>
+<?php include('navbar.php') // hier staan alle head variabelen, variabelen die standaard geinporteert moeten worden, beveiliging scripts enz. en de navbar?>
 
 
 <div id="banner">
-  <img src="http://www.centrumeenenal.nl/wp-content/uploads/2012/07/blije-mensen.jpg">
+  <img src="afbeeldingen/parlemetn.jpg">
 </div>
 
 <div class="container">
@@ -12,9 +24,9 @@
       <h3>Wat is Haagse Feiten?</h3>
       <p>Haagse Feiten is een digitale dienst die toegang biedt tot parlementaire informatie die verrijkt wordt met data en informatie uit diverse relevante bronnen. Bronnen zoals:<p>
 
-        <p>Overheidsbronnen <br>
-          Nieuwsbronnen;<br>
-          Social media-kanalen.
+        <p>- Overheidsbronnen <br>
+          - Nieuwsbronnen;<br>
+          - Social media-kanalen.
         </p>
 
         Je hoeft dus nooit meer tientallen websites te bezoeken, want alle relevante informatie wordt in één oogopslag helder en inzichtelijk gepresenteerd door middel van een persoonlijk dashboard.<br>
--- navbar.php
+++ navbar.php
@@ -1,24 +1,52 @@
-<!DOCTYPE html>
-<HTML>
-<HEAD>
-<TITLE>Welkom</TITLE>
-<link rel="stylesheet" type="text/css" href='css/index.css'>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-</head>
-</HEAD>
-<BODY bgcolor="#ffffff">
-  <nav class='navbar navbar-default'>
-        <div class='container-fluid'>
-          <div class='navbar-header'>
-            <a class='navbar-brand' href='#'>Haagse Feiten</a>
-          </div>
-          <ul class='nav navbar-nav'>
-            <li class='active'><a href='#'>Home</a></li>
-          <li><a href='#'>Page 1</a></li>
-          <li><a href='#'>Contact</a></li>
-          <li><a href='#'>Page 3</a></li>
-        </ul>
-      </div>
-    </nav>
+  <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php">
+        <img style="max-width:100px; margin-top: -15px;" alt="Brand" id="logo" src="afbeeldingen/logo.jpg">
+      </a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Informatie zoeken<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Zoeken op categorie</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Meest bekeken artikelen</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Leuke ideeën voor presentaties</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Contact</a></li>
+        <li class="dropdown">
+       <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1
+       <span class="caret"></span></a>
+       <ul class="dropdown-menu">
+         <li><a href="#">Page 1-1</a></li>
+         <li><a href="#">Page 1-2</a></li>
+         <li><a href="#">Page 1-3</a></li>
+       </ul>
+     </li>
+      </ul>
+      <form class="navbar-form navbar-right">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Zoeken">
+        </div>
+        <button type="submit" class="btn btn-default">Zoek</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Registreren</a></li>
+        <li><a href="#">Inloggen</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
