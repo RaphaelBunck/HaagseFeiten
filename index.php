545625c7fa517c33885755a5b29b7a4dc44d8d66
Majorfear <guidohovens@ictindewolkenalmere.nl>
2016-11-28 18:13:56 +0100

    index.php mooier
    .

--- index.php
+++ index.php
@@ -29,34 +29,26 @@ include('libraries/navbar.php'); // hier staan alle head variabelen, variabelen 
 </div>
 </div>
 
-<div class="container">
-
-
-  <div class="row">
-    <div class="col-sm-6 main" id="summary">
+<div id="wrapper">
+    <div id="summary">
       <h3>Wat is Haagse Feiten?</h3>
-      <p>Haagse Feiten is een digitale dienst die toegang biedt tot parlementaire informatie die verrijkt wordt met data en informatie uit diverse relevante bronnen. Bronnen zoals:<p>
-
-        <p>- Overheidsbronnen <br>
+      <p>Haagse Feiten is een digitale dienst die toegang biedt tot parlementaire informatie die verrijkt wordt met data en informatie uit diverse relevante bronnen. Bronnen zoals:<br/>
+          - Overheidsbronnen <br>
           - Nieuwsbronnen;<br>
-          - Social media-kanalen.
-        </p>
-
-        Je hoeft dus nooit meer tientallen websites te bezoeken, want alle relevante informatie wordt in één oogopslag helder en inzichtelijk gepresenteerd door middel van een persoonlijk dashboard.<br>
-
-        Wij richten ons op bedrijven, instellingen, overheden en professionals voor wie de politieke besluitvorming én het proces van invloed zijn op hun business, beleid of praktijk. Haagse Feiten faciliteert om dit reilen en zeilen goed te kunnen monitoren, dan wel te beïnvloeden.<br>
-
-        Haagse Feiten is dé toegangspoort waarachter alle beschikbare overheidsinformatie eenvoudig en snel terug te vinden is.</p>
-      </div>
+          - Social media-kanalen.<br/><br/>
+          Je hoeft dus nooit meer tientallen websites te bezoeken, want alle relevante informatie wordt in één oogopslag helder en inzichtelijk gepresenteerd door middel van een persoonlijk dashboard.<br/>
+          Wij richten ons op bedrijven, instellingen, overheden en professionals voor wie de politieke besluitvorming én het proces van invloed zijn op hun business, beleid of praktijk. Haagse Feiten faciliteert om dit reilen en zeilen goed te kunnen monitoren, dan wel te beïnvloeden.<br/>
+          Haagse Feiten is dé toegangspoort waarachter alle beschikbare overheidsinformatie eenvoudig en snel terug te vinden is.
+      </p>
+    </div>
 
 
-      <div class="col-sm-6 main" id="fom">
+      <div id="fom">
         <h3>Feit van de maand!</h3>
         <img src="https://haagsefeiten.nl/wp-content/uploads/2016/05/marcelHenriquez.jpg">
         <p><b>Wist je dat:</b> dit de oprichter is van Haagse Feiten?</p>
       </div>
     </div>
-  </div>
 
   <div class="footer">
     <div class="container-fluid">
